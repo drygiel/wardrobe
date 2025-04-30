158e029f30898971204bfaf432cf526aa163f34f
Damian Drygiel <damian.drygiel@santanderconsumer.dk>
2025-04-30 15:07:36 +0200

    add intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 // import styles from "./page.module.scss";
 import { AppProvider } from "@/contexts/AppContext";
-// import Intro from "@/Intro";
+import Intro from "@/Intro";
 import App from "@/App";
 
 export default function Home() {
   return (
     <AppProvider>
-      {/*<Intro />*/}
+      <Intro />
       <section>
         <App />
       </section>
